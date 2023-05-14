f01fffaafa8f58fc431be5dedbb30cdaddcedbbc
krypton <madhusudanbabar@gmail.com>
2023-05-14 21:00:39 +0530

    assistant

--- api/controllers/assistant.js
+++ api/controllers/assistant.js
@@ -4,22 +4,20 @@ const { openai } = require("../openai/index");
 
 app.handle("test_handle", (conv) => {
   console.log(conv);
-  if (conv.intent.name == "query") {
-    openai
-      .createCompletion({
-        prompt: conv.intent.query,
-      })
-      .then((data) => {
-        console.log(data);
-        conv.add(data);
-      })
-      .catch((e) => {
-        console.error(e);
-        conv.add(
-          `uh, oh! Something went wrong, error: ${e.response.status}, ${e.response.statusText}`
-        );
-      });
-  }
+  openai
+    .createCompletion({
+      prompt: conv.intent.query,
+    })
+    .then((data) => {
+      console.log(data);
+      conv.add(data);
+    })
+    .catch((e) => {
+      console.error(e);
+      conv.add(
+        `uh, oh! Something went wrong, error: ${e.response.status}, ${e.response.statusText}`
+      );
+    });
 });
 
 module.exports = { assistant: app };
